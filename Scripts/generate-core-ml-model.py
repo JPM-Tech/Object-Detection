2aa642557a7e431a5625f1aae6c0bd5dc3042035
Chase L <chase.lewis@daveramsey.com>
2021-05-10 17:45:54 -0500

    update create-coreml file

--- Scripts/generate-core-ml-model.py
+++ Scripts/generate-core-ml-model.py
@@ -1,12 +1,11 @@
 import os
 import sys
 import coremltools
-# import tensorflow
 
 #path_to_tf_model = input("Enter the directory path to the TensorFlow Model (the folder containing the .pb file): ")
 path_to_tf_model = "Functions-For-TensorFlow-and-TensorFlow-Lite-Model/checkpoints/custom-416"
 
-print("Creating TensorFlow Model")
+print("Creating CoreML Model")
 mlmodel = coremltools.convert(path_to_tf_model.rstrip())
 
 mlmodel.save(path_to_tf_model.rstrip() + ".mlmodel")
