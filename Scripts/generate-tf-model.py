0f8254e163c6d89ad186ea618050cd37684b349e
Chase L <chase.lewis@daveramsey.com>
2021-05-15 16:22:39 -0500

    generate an accurate tf-lite model

--- Scripts/generate-tf-model.py
+++ Scripts/generate-tf-model.py
@@ -12,9 +12,11 @@ os.chdir("Functions-For-TensorFlow-and-TensorFlow-Lite-Model/")
 print("Creating TensorFlow Model")
 os.system("python3 save_model.py --weights " + path_to_weights_file.rstrip() + " --output ./checkpoints/custom-416 --input_size 416 --model yolov" + yolo_version)
 
+# Removing this for now, it would require us to train a new model (*.weights file) using the *-tiny.weights weights with our images
 print("Creating TensorFlow Light Model")
-os.system("python3 save_model.py --weights " + path_to_weights_file.rstrip() + " --output ./checkpoints/custom-416-tf-lite --input_size 416 --model yolov" + yolo_version + " --framework tflite")
+# This will actually just convert our existing tf model to a tf-lite model
+os.system("python3 convert_tflite.py --weights " + path_to_weights_file.rstrip() + " --output ./checkpoints/custom-416.tflite")
 
 print("TensorFlow model created successfully")
 print("Path to created model...")
-open("Functions-For-TensorFlow-and-TensorFlow-Lite-Model/checkpoints/custom-416")
+print("Functions-For-TensorFlow-and-TensorFlow-Lite-Model/checkpoints/custom-416")
