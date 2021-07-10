0cfe152deaaa08312e0d97068aaed0c3db3b4c8b
Chase L <chase.lewis@daveramsey.com>
2021-07-10 18:58:44 -0500

    remove unused tfrecord code from file

--- Scripts/generate_tfrecord.py
+++ Scripts/generate_tfrecord.py
@@ -17,10 +17,8 @@ optional arguments:
 """
 
 import os
-import glob
 import pandas as pd
 import io
-import xml.etree.ElementTree as ET
 import argparse
 
 import re
@@ -118,28 +116,6 @@ def xml_to_csv(path):
                   'class', 'xmin', 'ymin', 'xmax', 'ymax']
   xml_df = pd.DataFrame(xml_list, columns=column_name)
   return xml_df
-  
-# This is from the original google code, it is fast, but not robust.
-# If the file it is parsing is not setup exactly as expected, the script fails.
-# I am commenting this out in favor of making the parser more friendly to the end user.
-  # for xml_file in glob.glob(path + '/*.xml'):
-  #   tree = ET.parse(xml_file)
-  #   root = tree.getroot()
-  #   for member in root.findall('object'):
-  #     value = (root.find('filename').text,
-  #               int(root.find('size')[0].text),
-  #               int(root.find('size')[1].text),
-  #               member[0].text,
-  #               int(member[4][0].text),
-  #               int(member[4][1].text),
-  #               int(member[4][2].text),
-  #               int(member[4][3].text)
-  #             )
-  #     xml_list.append(value)
-  # column_name = ['filename', 'width', 'height',
-  #                 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-  # xml_df = pd.DataFrame(xml_list, columns=column_name)
-  # return xml_df
 
 
 def class_text_to_int(row_label):
