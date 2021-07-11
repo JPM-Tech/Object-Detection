da283b8183150f40da3fb46d2bb7390a06132f0e
Chase L <chase.lewis@daveramsey.com>
2021-07-11 14:29:55 -0500

    fix training issue, add readme for converters, update yolo to xml script

---
+++ Scripts/converters/README.md
@@ -0,0 +1,45 @@
+# Converters
+
+## Convert Yolo to XML
+In order to install and run this script you will only need two dependencies: Python version 3 or higher, and PIL.
+
+PIL is a python based library that makes working with images much easier. PIL can be installed by running the following two commands:
+```
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+```
+
+### Running the Yolo to XML Script
+Once the dependencies are installed, change into the directory that holds the script.
+
+The script will ask you for the path to the folder that holds both the images and .txt yolo format (these are expected to exist in the same folder).
+
+It will also ask for the path to the class names. This file is usally named something like "classes.txt" or "obj.names"
+
+Once the script has run, it will create a new folder in the same directory that holds the images that is called "XML". A new folder is created to prevent any issues where a program like "LabelImg" may have trouble firguring out what format to save any newly labeled images in.
+
+Running the following command will run the script and from there, start converting your files:
+```
+python3 convert-yolo-to-xml.py
+```
+
+
+## Resize Images
+In order to install and run this script you will only need two dependencies: Python version 3 or higher, and PIL.
+
+PIL is a python based library that makes working with images much easier. PIL can be installed by running the following two commands:
+```
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+```
+
+If you want to resize your images to save on storage space or speed up load times while using online GPU solutions like CoLab. You can use this script to shrink the size of the image, all while keeping the correct aspect ratios so that images aren't distorted. It will also automatically convert all the files that are run through this script to a ".jpeg" format.
+
+This will ask you for the folder containing the images you want to resize (it is best if they are first copied into a new folder from the original images, incase you decide you still want to use the full scale images at some point down the road).
+
+The next thing it will ask you for is the maximum size (in pixels) that you want to images to be. Note, the aspect ratio will be kept so that the images don't look distorted.
+
+Running the following command will run the script and from there, start converting your files:
+```
+python3 resize-images.py
+```
--- Scripts/converters/convert-yolo-to-xml.py
+++ Scripts/converters/convert-yolo-to-xml.py
@@ -3,8 +3,12 @@ import re
 from PIL import Image
 
 folder_holding_yolo_files = input("Enter the path to the yolo files: ").replace("'", "").strip()
-# TODO: Make a func that compaires the number in the yolo file to the corresponding name from the obj.names file
-object_name = 'license_plate'
+yolo_class_list_file = input("Enter the path to the file that has the yolo classes (typically classes.txt): ").strip()
+
+# Get a list of all the classes used in the yolo format
+with open(yolo_class_list_file) as f:
+    yolo_classes = f.readlines()
+array_of_yolo_classes = [x.strip() for x in yolo_classes]
 
 # Description of Yolo Format values
 # 15 0.448743 0.529142 0.051587 0.021081
@@ -27,10 +31,19 @@ for each_yolo_file in os.listdir(folder_holding_yolo_files):
   if each_yolo_file.endswith("txt"):
     the_file = open(each_yolo_file, 'r')
     all_lines = the_file.readlines()
+    image_name = each_yolo_file
 
     # Check to see if there is an image that matches the txt file
-    image_name = each_yolo_file.replace('txt', 'jpeg')
-    if os.path.exists(image_name):
+    if os.path.exists(each_yolo_file.replace('txt', 'jpeg')):
+      image_name = each_yolo_file.replace('txt', 'jpeg')
+    if os.path.exists(each_yolo_file.replace('txt', 'jpg')):
+      image_name = each_yolo_file.replace('txt', 'jpg')
+    if os.path.exists(each_yolo_file.replace('txt', 'png')):
+      image_name = each_yolo_file.replace('txt', 'png')
+
+    if not image_name == each_yolo_file:
+      # If the image name is the same as the yolo filename
+      # then we did NOT find an image that matches, and we will skip this code block
       orig_img = Image.open(image_name) # open the image
       image_width = orig_img.width
       image_height = orig_img.height
@@ -56,7 +69,7 @@ for each_yolo_file in os.listdir(folder_holding_yolo_files):
           yolo_array = re.split("\s", each_line.rstrip()) # remove any extra space from the end of the line
 
           # initalize the variables
-          class_number = 0.0
+          class_number = 0
           x_yolo = 0.0
           y_yolo = 0.0
           yolo_width = 0.0
@@ -73,6 +86,7 @@ for each_yolo_file in os.listdir(folder_holding_yolo_files):
             if yolo_array_contains_only_digits:
               # assign the variables
               class_number = int(yolo_array[0])
+              object_name = array_of_yolo_classes[class_number]
               x_yolo = float(yolo_array[1])
               y_yolo = float(yolo_array[2])
               yolo_width = float(yolo_array[3])
